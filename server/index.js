85f41dfe1eea90d5d1f309923348b99792c1d2c9
Fellipe Campanholo Moreira <fellipecm@gmail.com>
2020-06-30 03:06:50 +0000

    Server with CORS restrictions

--- server/index.js
+++ server/index.js
@@ -4,6 +4,16 @@ const debug = require("debug")("server");
 const app = express();
 const port = process.env.SERVER_PORT || 3001;
 
+// NEW - Add CORS headers - see https://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "^https://ui.fellipecm.com$");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/api/ping", (req, res) => {
   res.send({
     msg: "Hello, World"
